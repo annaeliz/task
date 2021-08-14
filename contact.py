7809197b4da085aeba8f543bf9f35191d48569e4
annaeliz <annelizabethleju@gmail.com>
2021-08-14 12:27:22 +0530

    phonebook

---
+++ contact.py
@@ -0,0 +1,69 @@
+def insertion(contacts):
+    i=input("Name: ")
+    j=input("Number: ")
+    contacts[i]=j
+
+def updation(contacts,i,j):
+    if i in contacts:
+        contacts.pop(i)
+        print("Enter new value of Name/Number of the Contact")
+        values=input("value: ")
+        if(values.isnumeric()):
+            contacts.update({i:values})
+            print("Number is Updated")
+        else:
+            contacts.update({values:j})
+            print("Name is updated")
+
+def remove(contacts):
+    n=input("Enter the contact to be removed:")
+    if n in contacts:
+        contacts.pop(n)
+    else:
+        print("Please Enter vaild contact")
+    
+def display(contacts):
+    print("Contact Book: ")
+    print("Name   Number")
+    for i,j in contacts.items():
+        print(i,"   ",j)
+    print("The list of Contact :")
+    print(sorted(contacts.items()))
+
+def countcontact(contacts):
+    n=0
+    for i in contacts.items():
+        n=n+1
+    print(n)
+
+def search(contacts):
+    s=input("Enter the Name to search: ")
+    if s in contacts:
+        print("Contact is Present")
+        print(s, contacts[s])
+    else:
+        print("Not Found")
+      
+            
+contact={}
+c=0
+while(c<7):
+    c=int(input("Enter the choice: "))
+    if(c==1):
+        insertion(contact)
+    elif(c==2):
+        n=input("Name of contact to be upadted: ")
+        m=contact[n]
+        updation(contact,n,m)
+    elif(c==3):
+        remove(contact)
+    elif(c==4):
+        display(contact)
+    elif(c==5):
+        countcontact(contact)
+    elif(c==6):
+        search(contact)
+    else:
+        print("exit")
+
+
